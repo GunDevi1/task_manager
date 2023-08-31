b8db21d3d320d4a7acb019c2686a1c1599a51afb
Aki Hayakawa <71726587+GunDevi1@users.noreply.github.com>
2023-09-01 01:15:13 +0300

    tm add an illustration to a project

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -2,10 +2,18 @@
 
 @section('content')
     <div class="container">
-        <div class="row content">
-            <div class="col-md-6">
+        <div class="row content d-flex align-items-center">
+            <div class="col-md-6 text-center">
                 <h1>Добро пожаловать на сайт Tusk</h1>
-                <p>Здесь вы сможете эффективно управлять своими задачами и планировать свои дела.</p>
+                <p>
+                    Здесь вы сможете эффективно управлять своими задачами и планировать свои дела. <br>
+                    Tusk обеспечивает безупречную организацию ваших задач. <br>
+                    Создавайте список дел,
+                    присваивайте им приоритеты, устанавливайте сроки выполнения и отслеживайте прогресс.
+                </p>
+            </div>
+            <div class="col-md-6">
+                <img src="{{ asset('images/checklist.png') }}" alt="man_holding_pencil" class="img-fluid">
             </div>
         </div>
     </div>
